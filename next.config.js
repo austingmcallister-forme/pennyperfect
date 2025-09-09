228f34273e5844b1320cbc3b4dec47c02f0fcc01
Austin McAllister <austinmcallister@MacBookPro.lan>
2025-09-09 17:21:06 -0500

    Fix Next.js config and add Prisma generate to build process

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ['cdn.shopify.com'],
   },
-  env: {
-    SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
-    SHOPIFY_API_SECRET: process.env.SHOPIFY_API_SECRET,
-    SHOPIFY_SCOPES: process.env.SHOPIFY_SCOPES,
-    APP_URL: process.env.APP_URL,
-    ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
-  },
 }
 
 module.exports = nextConfig
